7046c7204418ef57c8c1c088fb624245b687bf90
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2024-10-28 10:59:50 +0100

    add initial terraform config

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group.name
+  location = var.resource_group.location
+  tags = merge(
+    try(var.tags),
+    tomap({
+      "Resource Type" = "Resource Group"
+    })
+  )
+}
+
+resource "azurerm_private_dns_zone" "this" {
+  for_each            = local.private_dns_zones
+  name                = each.key
+  resource_group_name = azurerm_resource_group.this.name
+  tags = merge(
+    try(var.tags),
+    tomap({
+      "Resource Type" = "Private DNS Zone"
+    })
+  )
+}
