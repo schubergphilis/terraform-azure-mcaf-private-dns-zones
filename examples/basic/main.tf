0bfb653535f3bce173983ccf2083636a85728231
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2024-11-04 09:01:42 +0100

    modified example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,17 @@
 terraform {
   required_version = ">= 1.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  features {}
 }
 
 module "private_dns_zones" {
