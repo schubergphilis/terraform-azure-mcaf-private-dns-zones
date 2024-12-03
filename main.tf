6b716e8620d710789eb2aa115aa405491c791e55
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2024-12-03 08:47:14 +0100

    tags testing

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "azurerm_private_dns_zone" "this" {
   for_each            = local.private_dns_zones
   name                = each.key
   resource_group_name = azurerm_resource_group.this.name
-  tags = merge(
-    try(var.tags),
-    tomap({
-      "Resource Type" = "Private DNS Zone"
-    })
-  )
+  # tags = merge(
+  #   try(var.tags),
+  #   tomap({
+  #     "Resource Type" = "Private DNS Zone"
+  #   })
+  # )
 }
