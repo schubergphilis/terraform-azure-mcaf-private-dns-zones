18b421ce2d1e9a06f006034ad539579a4ae271eb
Gillian Stravers <gillianstravers@icloud.com>
2025-01-22 10:25:11 +0100

    add virtual network link block

--- main.tf
+++ main.tf
@@ -13,10 +13,29 @@ resource "azurerm_private_dns_zone" "this" {
   for_each            = local.private_dns_zones
   name                = each.key
   resource_group_name = azurerm_resource_group.this.name
+
   tags = merge(
     var.tags,
     tomap({
       "Resource_Type" = "Private DNS Zone"
+    })
+  )
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "this" {
+  count               = var.virtual_network_id != null ? 1 : 0
+  for_each            = local.private_dns_zones
+  name                = "${each.key}-vnet-link"
+  resource_group_name = azurerm_resource_group.this.name
+  private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
+  virtual_network_id  = var.virtual_network_id
+
+  depends_on = [azurerm_private_dns_zone.this]
+
+  tags = merge(
+    var.tags,
+    tomap({
+      "Resource_Type" = "Private DNS Zone Virtual Network Link"
     })
   )
 }
