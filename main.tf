9a3559edf312deb0cd462d6160e06ced8c64ecca
Gillian Stravers <gillianstravers@icloud.com>
2025-01-22 12:18:30 +0100

    format

--- main.tf
+++ main.tf
@@ -23,11 +23,11 @@ resource "azurerm_private_dns_zone" "this" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  for_each            = var.virtual_network_id != null ? local.private_dns_zones : {}
-  name                = "${each.key}-vnet-link"
-  resource_group_name = azurerm_resource_group.this.name
+  for_each              = var.virtual_network_id != null ? local.private_dns_zones : {}
+  name                  = "${each.key}-vnet-link"
+  resource_group_name   = azurerm_resource_group.this.name
   private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
-  virtual_network_id  = var.virtual_network_id
+  virtual_network_id    = var.virtual_network_id
 
   depends_on = [azurerm_private_dns_zone.this]
 
