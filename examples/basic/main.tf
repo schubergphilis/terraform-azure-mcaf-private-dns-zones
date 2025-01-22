ffaa51acc255c953ca5a3f010b28b62c59f4711e
Gillian Stravers <gillianstravers@icloud.com>
2025-01-22 12:10:16 +0100

    update example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -23,6 +23,7 @@ module "private_dns_zones" {
 
   location      = "West Europe"
   shortlocation = "weu"
+  virtual_network_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/example-rg/providers/Microsoft.Network/virtualNetworks/example-vnet"
 
   # some sql database types need a dedicated DNS zone, supply them in a seperate list, i.e.
   additional_zones = ["privatelink.a1b2c3d4e5f6.database.windows.net"]
