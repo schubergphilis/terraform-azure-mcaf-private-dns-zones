08a8d8b3e493579d054bf22bd596aeaea88f27a4
rwiankowski <rwiankowski@mac.com>
2025-03-03 17:12:05 +0100

    Added Resource Group ID to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "private_dns_zones" {
   description = "object with all created Private DNS Zones"
   value       = azurerm_private_dns_zone.this
 }
+
+output "resource_group_id" {
+  description = "ID of the Resource Group created by the module"  
+  value       = azurerm_resource_group.this.id
+}
