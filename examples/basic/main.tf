a1b6ad0e51307b68dd85602c30f7b831c6857ba2
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-03-04 13:20:54 +0100

    fix example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -17,10 +17,7 @@ provider "azurerm" {
 module "private_dns_zones" {
   source = "../.."
 
-  resource_group = {
-    name = "example-rg"
-  }
-
+  resource_group_name "example-rsg"
   location           = "West Europe"
   shortlocation      = "weu"
   virtual_network_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/example-rg/providers/Microsoft.Network/virtualNetworks/example-vnet"
